ddfa592af3107913d5fa6fc3d49fefc67e1d7733
rainshadower <47715830+rainshadower@users.noreply.github.com>
2025-09-20 20:40:30 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -16,6 +16,36 @@ const CUSTOMER_SITES = {
         api: 'https://tyyszy.com/api.php/provide/vod',
         name: '天涯资源',
     },
+     ckzy: {
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源'
+    },
+    jkun: {
+        api: 'https://jkunzyapi.com',
+        name: 'jkun资源'
+    },
+    bwzy: {
+        api: 'https://api.bwzym3u8.com',
+        name: '百万资源'
+    },
+    souav: {
+        api: 'https://api.souavzy.vip',
+        name: 'souav资源',
+        adult: true
+    },
+    r155: {
+        api: 'https://155api.com',
+        name: '155资源'
+    },
+    lsb: {
+        api: 'https://apilsbzy1.com',
+        name: 'lsb资源'
+    },
+    huangcang: {
+        api: 'https://hsckzy.vip',
+        name: '黄色仓库',
+        detail: 'https://hsckzy.vip'
+    },
     xiaomaomi: {
         api: 'https://zy.xmm.hk/api.php/provide/vod',
         name: '小猫咪资源',
@@ -94,37 +124,7 @@ const CUSTOMER_SITES = {
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源'
-    },
-    ckzy: {
-        api: 'https://www.ckzy1.com',
-        name: 'CK资源'
-    },
-    jkun: {
-        api: 'https://jkunzyapi.com',
-        name: 'jkun资源'
-    },
-    bwzy: {
-        api: 'https://api.bwzym3u8.com',
-        name: '百万资源'
-    },
-    souav: {
-        api: 'https://api.souavzy.vip',
-        name: 'souav资源',
-        adult: true
-    },
-    r155: {
-        api: 'https://155api.com',
-        name: '155资源'
-    },
-    lsb: {
-        api: 'https://apilsbzy1.com',
-        name: 'lsb资源'
-    },
-    huangcang: {
-        api: 'https://hsckzy.vip',
-        name: '黄色仓库',
-        detail: 'https://hsckzy.vip'
-    },
+    },    
     yutu: {
         api: 'https://yutuzy10.com',
         name: '玉兔资源'
